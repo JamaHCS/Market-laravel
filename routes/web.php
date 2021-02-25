65eee84e3aac5ef32bc5e1bcd8f60190f9a7150a
JamaHCS <jama117@hotmail.com>
2021-02-25 02:11:47 -0600

    [Add] Basic social login with facebook.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\SocialController;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
@@ -12,3 +14,26 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('statistics/{market}', [ProductController::class, 'soldProducts'])->name('statistics');
+
+
+
+// Route::get('/auth/facebook', function () {
+//     return Socialite::driver('facebook')->redirect();
+// });
+
+// Route::get('/auth/facebook/callback', function () {
+//     $user = Socialite::driver('facebook')->user();
+//     // $user->token
+// });
+
+// Route::get('/auth/google', function () {
+//     return Socialite::driver('facebook')->redirect();
+// });
+
+// Route::get('/auth/google/callback', function () {
+//     $user = Socialite::driver('google')->user();
+//     // $user->token
+// });
+
+Route::get('auth/facebook', [SocialController::class, 'facebookRedirect']);
+Route::get('auth/facebook/callback', [SocialController::class, 'loginWithFacebook']);
