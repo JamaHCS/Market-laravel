cca4a6dcfdf50f34657b7c3b144ad7d2ac16f85f
JamaHCS <jama117@hotmail.com>
2021-02-26 02:33:46 -0600

    [PAD] Sell migration

--- database/factories/SellFactory.php
+++ database/factories/SellFactory.php
@@ -24,7 +24,6 @@ class SellFactory extends Factory
         return [
             'market_id' => 1,
             'user_id' => 1,
-            'month' => $this->faker->numberBetween(1, 12)
         ];
     }
 }
