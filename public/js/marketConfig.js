f9bb3309cde67941ae512076ee01878e58ec1ed5
JamaHCS <jama117@hotmail.com>
2021-03-09 14:49:56 -0600

    [Add] Updated animation added

--- public/js/marketConfig.js
+++ public/js/marketConfig.js
@@ -1,12 +1,18 @@
 const logoInput = document.getElementById("logo-input");
+const updated = document.getElementById("updated");
 
 console.log("Config");
 
 const sendingData = async () => {
   let data = new FormData(document.getElementById("basic-conf"));
   console.log(data, "okay data");
+
   let response = await sendingRequest(data);
   console.log(response);
+
+  if (response) {
+    updateAnimation();
+  }
 };
 
 const sendingRequest = async (data) => {
@@ -14,12 +20,23 @@ const sendingRequest = async (data) => {
     let req = await fetch(`${URL_SITE}/api/v1/markets/basic-config`, {
       method: "POST",
       body: data,
+    }).then((response) => {
+      updated.classList.add("show");
+      return response;
     });
+
     console.log(req);
     let json = await req.json();
+
     return json;
   } catch (err) {
     console.log(err);
     throw Error(err);
   }
+};
+
+const updateAnimation = () => {
+  setTimeout(() => {
+    updated.classList.remove("show");
+  }, 4000);
 };
--- resources/views/markets/config.blade.php
+++ resources/views/markets/config.blade.php
@@ -94,6 +94,7 @@
             @csrf
 
             <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+              <span id="updated" class="text-muted mr-4 updated">Actualizado!</span>
               <button type="button" id="btn-basic-conf" onclick="sendingData()" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
                 {{ __('Actualizar') }}
               </button>
